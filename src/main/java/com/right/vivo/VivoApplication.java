507d436afaa653f539480808dc7d62e75402123a
DW <1121057486@qq.com>
2019-05-26 11:04:14 +0800

    basic

---
+++ src/main/java/com/right/vivo/VivoApplication.java
@@ -0,0 +1,16 @@
+package com.right.vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DW
+ */
+@SpringBootApplication
+public class VivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivoApplication.class, args);
+    }
+
+}
