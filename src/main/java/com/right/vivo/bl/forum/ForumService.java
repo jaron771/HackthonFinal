1289906797a4118242911d2ced8be6d78010f155
171250517 <171250517@smail.nju.edu.cn>
2019-05-26 16:43:59 +0800

    forum相关

--- src/main/java/com/right/vivo/bl/forum/ForumService.java
+++ src/main/java/com/right/vivo/bl/forum/ForumService.java
@@ -1,5 +1,53 @@
 package com.right.vivo.bl.forum;
 
+import com.right.vivo.po.Post;
+import com.right.vivo.vo.PostForm;
+import com.right.vivo.vo.ResponseVO;
+
+import java.util.List;
+
+
 public interface ForumService {
 
+    /**
+     * 根据专业id获取论坛id
+     * @param majorId
+     * @return
+     */
+    int getForumIdByMajorId(int majorId);
+
+    /**
+     * 获取论坛的帖子
+     * @param forumId
+     * @return
+     */
+    List<Post> getPostsByForumId(int forumId);
+
+    /**
+     * 获取帖子的回帖
+     * @param postId
+     * @return
+     */
+    List<Post> getRepliesByPostId(int postId);
+
+    /**
+     * 发帖
+     * @param postForm
+     * @return
+     */
+    void addPost(PostForm postForm);
+
+    /**
+     * 删帖
+     * @param postId
+     * @return
+     */
+    void deletePost(int postId);
+
+    /**
+     * 更新帖
+     * @param postForm
+     * @return
+     */
+    void updatePost(PostForm postForm);
 }
--- src/main/java/com/right/vivo/data/forum/ForumMapper.java
+++ src/main/java/com/right/vivo/data/forum/ForumMapper.java
@@ -1,8 +1,28 @@
 package com.right.vivo.data.forum;
 
+import com.right.vivo.po.Forum;
+import com.right.vivo.po.Post;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * @author DW
  * @date 2019/5/26
  */
+@Mapper
 public interface ForumMapper {
+    Forum selectForumByMajorId(@Param("majorId") int majorId);
+
+    List<Post> selectPostsByForumId(@Param("forumId") int forumId);
+
+    List<Post> selectRepliesByPostId(@Param("postId") int postId);
+
+    void insertPost(Post post);
+
+    void deletePost(@Param("postId") int postId);
+
+    void updatePost(Post post);
+
 }
