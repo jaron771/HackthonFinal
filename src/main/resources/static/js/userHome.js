2d7c985bcb466f098dc45c41bde49f4224861152
171250519 <171250519@smail.nju.edu.cn>
2019-05-27 10:26:03 +0800

    去掉logo，userhome大概写好了，但是有bug

--- src/main/resources/static/js/userHome.js
+++ src/main/resources/static/js/userHome.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
+    getRequest(
+        "/user/get/" + sessionStorage.getItem('id'),
+        function (res) {
+            var user = res.content;
+            $("#name").innerHTML = user.name;
+            getRequest(
+                "/getSchoolInfo/" + user.universityId,
+                function (res) {
+                    $("#university").innerHTML = res.content.university.name;
+                },
+                function (error) {
+                    alert(error);
+                }
+            );
+            getRequest(
+                "/getMajorInfo/" + user.majorId,
+                function (res) {
+                    $("#major").innerHTML = res.content.name;
+                },
+                function (error) {
+                    alert(error);
+                }
+            )
 
+        }
+    )
 });
