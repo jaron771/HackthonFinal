44e21e8a7a871a2057b30f83c7104b9f4c4b7b94
DW <1121057486@qq.com>
2019-05-27 14:48:17 +0800

    个人中心

--- src/main/resources/static/js/universityInfo.js
+++ src/main/resources/static/js/universityInfo.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
     let universityId = $("#universityId").text();
-    console.log(universityId);
+
     getRequest(
         "/search/get/SchoolInfo/" + universityId,
         function (res) {
-            $("#brief-introduce").innerHTML = res.content.university.briefIntro;
-            for (var i = 0; i < 9; i++) {
-                $("#major" + i).innerHTML = res.content.majors[i].name;
-                //TODO:这里把按钮的href指定为该专业的详情页
+            console.log(res);
+            $('.department').empty();
+            let majorDomStr = '';
+
+            for (let k = 0; k < res.content.majors.length;) {
+                majorDomStr += "<div class=\"row\">\n";
+                for (let l = 0; l < 3 && k < res.content.majors.length; l++) {
+                    majorDomStr += "<div class=\'col-md-3 offset-1\'>\n" +
+                        "    <a class=\'btn btn-success\' role=\'button\' href=\'/major/forum?majorId=" + res.content.majors[k].id + "\'>" + res.content.majors[k].name + "</a>\n" +
+                        "</div>\n";
+                    k++;
+                }
+                majorDomStr += "</div>";
             }
+            console.log(majorDomStr);
+            $('.department').append(majorDomStr);
         },
         function (error) {
             alert(error);
--- src/main/resources/static/js/userHome.js
+++ src/main/resources/static/js/userHome.js
@@ -3,26 +3,33 @@ $(document).ready(function () {
         "/user/get/" + sessionStorage.getItem('id'),
         function (res) {
             var user = res.content;
-            $("#name").innerHTML = user.name;
-            getRequest(
-                "/getSchoolInfo/" + user.universityId,
-                function (res) {
-                    $("#university").innerHTML = res.content.university.name;
-                },
-                function (error) {
-                    alert(error);
-                }
-            );
-            getRequest(
-                "/getMajorInfo/" + user.majorId,
-                function (res) {
-                    $("#major").innerHTML = res.content.name;
-                },
-                function (error) {
-                    alert(error);
-                }
-            )
-
+            console.log(user);
+            document.getElementById("name").innerHTML = "姓名：" + user.username;
+            if (user.universityId != null) {
+                getRequest(
+                    "/search/get/SchoolInfo/" + user.universityId,
+                    function (res) {
+                        document.getElementById("university").innerHTML = "大学：" + res.content.university.name;
+                    },
+                    function (error) {
+                        alert(error);
+                    }
+                );
+            }
+            if (user.majorId != null) {
+                getRequest(
+                    "/search/getMajorInfo/" + user.majorId,
+                    function (res) {
+                        document.getElementById("major").innerHTML = "专业：" + res.content.name;
+                    },
+                    function (error) {
+                        alert(error);
+                    }
+                );
+            }
+        },
+        function (error) {
+            alert(error);
         }
     )
 });
